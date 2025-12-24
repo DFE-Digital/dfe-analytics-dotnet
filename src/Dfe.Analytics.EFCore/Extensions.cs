b8a5e85fed14e222dc324ce5193f56fb84a90f7f
James Gunn <james@gunn.io>
2025-12-24 17:07:53 +0000

    Add extension methods for configuring services

---
+++ src/Dfe.Analytics.EFCore/Extensions.cs
@@ -0,0 +1,52 @@
+using Dfe.Analytics.EFCore.AirbyteApi;
+using Dfe.Analytics.EFCore.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Dfe.Analytics.EFCore;
+
+public static class Extensions
+{
+    public static IHostApplicationBuilder AddDfeAnalyticsDeploymentTools(this IHostApplicationBuilder builder)
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+
+        builder.Services.AddDfeAnalyticsDeploymentTools();
+
+        builder.Services.Configure<AirbyteApiOptions>(options =>
+        {
+            builder.Configuration.GetSection("DfeAnalytics:Airbyte").Bind(options);
+        });
+
+        return builder;
+    }
+
+    public static IServiceCollection AddDfeAnalyticsDeploymentTools(this IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+
+        services.AddOptions<AirbyteApiOptions>()
+            .ValidateOnStart();
+
+        services
+            .AddSingleton<AirbyteApiClient>()
+            .AddSingleton<AnalyticsConfigurationProvider>()
+            .AddTransient<AnalyticsDeployer>();
+
+        var airbyteApiClientBuilder = services.AddHttpClient<AirbyteApiClient>();
+        AirbyteApiClient.ConfigureHttpClient(airbyteApiClientBuilder);
+
+        return services;
+    }
+
+    public static IServiceCollection ConfigureAirbyteApi(this IServiceCollection services, Action<AirbyteApiOptions> configureOptions)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(configureOptions);
+
+        services.Configure(configureOptions);
+
+        return services;
+    }
+}
