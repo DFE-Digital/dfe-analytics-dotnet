7e67a4a2e1dec789ab47590c18a5a28034461094
James Gunn <james@gunn.io>
2025-12-31 17:27:25 +0000

    Don't explode if service provider doesn't have IConfiguration

--- src/Dfe.Analytics.Core/DfeAnalyticsConfigureOptions.cs
+++ src/Dfe.Analytics.Core/DfeAnalyticsConfigureOptions.cs
@@ -2,30 +2,34 @@ using System.Text.Json;
 using Google.Apis.Auth.OAuth2;
 using Google.Cloud.BigQuery.V2;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
 namespace Dfe.Analytics;
 
-internal class DfeAnalyticsConfigureOptions(IConfiguration configuration) : IConfigureOptions<DfeAnalyticsOptions>
+internal class DfeAnalyticsConfigureOptions(IServiceProvider serviceProvider) : IConfigureOptions<DfeAnalyticsOptions>
 {
     public void Configure(DfeAnalyticsOptions options)
     {
         ArgumentNullException.ThrowIfNull(options);
 
-        var configurationSection = configuration.GetSection(Constants.ConfigurationSectionName);
+        if (serviceProvider.GetService<IConfiguration>() is { } configuration)
+        {
+            var configurationSection = configuration.GetSection(Constants.ConfigurationSectionName);
 
-        configurationSection.AssignConfigurationValueIfNotEmpty("DatasetId", v => options.DatasetId = v);
-        configurationSection.AssignConfigurationValueIfNotEmpty("ProjectId", v => options.ProjectId = v);
-        configurationSection.AssignConfigurationValueIfNotEmpty("Audience", v =>
-        {
-            options.FederatedAksAuthentication ??= new();
-            options.FederatedAksAuthentication.Audience = v;
-        });
-        configurationSection.AssignConfigurationValueIfNotEmpty("GenerateAccessTokenUrl", v =>
-        {
-            options.FederatedAksAuthentication ??= new();
-            options.FederatedAksAuthentication.ServiceAccountImpersonationUrl = v;
-        });
+            configurationSection.AssignConfigurationValueIfNotEmpty("DatasetId", v => options.DatasetId = v);
+            configurationSection.AssignConfigurationValueIfNotEmpty("ProjectId", v => options.ProjectId = v);
+            configurationSection.AssignConfigurationValueIfNotEmpty("Audience", v =>
+            {
+                options.FederatedAksAuthentication ??= new();
+                options.FederatedAksAuthentication.Audience = v;
+            });
+            configurationSection.AssignConfigurationValueIfNotEmpty("GenerateAccessTokenUrl", v =>
+            {
+                options.FederatedAksAuthentication ??= new();
+                options.FederatedAksAuthentication.ServiceAccountImpersonationUrl = v;
+            });
+        }
 
         if (!string.IsNullOrEmpty(options.CredentialsJson))
         {
