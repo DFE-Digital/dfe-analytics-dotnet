2071a51192936cf3cf78fb39f35e919d5b758c96
James Gunn <james@gunn.io>
2025-12-31 09:56:33 +0000

    Add metadata for column syncing including PII flag

--- src/Dfe.Analytics.EFCore/DbContextConfigurationExtensions.cs
+++ src/Dfe.Analytics.EFCore/DbContextConfigurationExtensions.cs
@@ -6,11 +6,22 @@ namespace Dfe.Analytics.EFCore;
 public static class DbContextConfigurationExtensions
 {
     // ReSharper disable once UnusedMethodReturnValue.Global
-    public static T HasAnalyticsSync<T>(this T builder, bool syncTable = true) where T : EntityTypeBuilder
+    public static T HasAnalyticsSync<T>(this T builder, bool syncTable = true, bool syncAllColumns = true, bool? columnsArePii = null)
+        where T : EntityTypeBuilder
     {
         ArgumentNullException.ThrowIfNull(builder);
 
-        builder.HasAnnotation(AnnotationKeys.TableAnalyticsSyncMetadata, new TableSyncMetadata(syncTable));
+        builder.HasAnnotation(AnnotationKeys.TableAnalyticsSyncMetadata, new TableSyncMetadata(syncTable, syncAllColumns, columnsArePii));
+
+        return builder;
+    }
+
+    public static T HasAnalyticsSync<T>(this T builder, bool syncColumn = true, bool? isPii = null)
+        where T : PropertyBuilder
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+
+        builder.HasAnnotation(AnnotationKeys.ColumnAnalyticsSyncMetadata, new ColumnSyncMetadata(syncColumn, isPii));
 
         return builder;
     }
