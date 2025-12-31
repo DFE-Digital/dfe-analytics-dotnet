7cea045c2586bfca53f21984cd40f209a00e95a7
James Gunn <james@gunn.io>
2025-12-31 09:56:33 +0000

    Add metadata for column syncing including PII flag

--- src/Dfe.Analytics.EFCore/Description/TableSyncMetadata.cs
+++ src/Dfe.Analytics.EFCore/Description/TableSyncMetadata.cs
@@ -1,3 +1,3 @@
 namespace Dfe.Analytics.EFCore.Description;
 
-internal record TableSyncMetadata(bool SyncTable);
+internal record TableSyncMetadata(bool SyncTable, bool SyncAllColumns, bool? ColumnsArePii);
