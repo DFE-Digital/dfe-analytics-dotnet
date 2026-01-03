a33ae617c31c56c82465017d6a9b565936750ef2
James Gunn <james@gunn.io>
2026-01-03 12:20:50 +0000

    Fix assigning CredentialsJson from config
    Also combines the IConfigureOptions<> and IPostConfigureOptions<> for DfeAnalyticsOptions into a single class.

--- src/Dfe.Analytics.Core/DfeAnalyticsConfigureOptions.cs
+++ src/Dfe.Analytics.Core/DfeAnalyticsConfigureOptions.cs
@@ -1,10 +1,15 @@
+using System.Text.Json;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.BigQuery.V2;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
 namespace Dfe.Analytics;
 
-internal class DfeAnalyticsConfigureOptions(IServiceProvider serviceProvider) : IConfigureOptions<DfeAnalyticsOptions>
+internal class DfeAnalyticsConfigureOptions(IServiceProvider serviceProvider) :
+    IConfigureOptions<DfeAnalyticsOptions>,
+    IPostConfigureOptions<DfeAnalyticsOptions>
 {
     public void Configure(DfeAnalyticsOptions options)
     {
@@ -16,6 +21,7 @@ internal class DfeAnalyticsConfigureOptions(IServiceProvider serviceProvider) : 
 
             configurationSection.AssignConfigurationValueIfNotEmpty("DatasetId", v => options.DatasetId = v);
             configurationSection.AssignConfigurationValueIfNotEmpty("ProjectId", v => options.ProjectId = v);
+            configurationSection.AssignConfigurationValueIfNotEmpty("CredentialsJson", v => options.CredentialsJson = v);
             configurationSection.AssignConfigurationValueIfNotEmpty("Audience", v =>
             {
                 options.FederatedAksAuthentication ??= new();
@@ -26,6 +32,66 @@ internal class DfeAnalyticsConfigureOptions(IServiceProvider serviceProvider) : 
                 options.FederatedAksAuthentication ??= new();
                 options.FederatedAksAuthentication.ServiceAccountImpersonationUrl = v;
             });
+        }
+    }
+
+    public void PostConfigure(string? name, DfeAnalyticsOptions options)
+    {
+        if (string.IsNullOrEmpty(options.CredentialsJson))
+        {
+            return;
+        }
+
+        // Configure missing properties from the credentials JSON if it's set
+
+        using var credentialsJson = JsonDocument.Parse(options.CredentialsJson);
+
+        if (options.ProjectId is null &&
+            credentialsJson.RootElement.TryGetProperty("project_id", out var projectIdElement))
+        {
+            options.ProjectId = projectIdElement.GetString();
+        }
+
+        if (options.FederatedAksAuthentication?.Audience is null &&
+            credentialsJson.RootElement.TryGetProperty("audience", out var audienceElement))
+        {
+            options.FederatedAksAuthentication ??= new();
+            options.FederatedAksAuthentication.Audience = audienceElement.GetString()!;
+        }
+
+        if (options.FederatedAksAuthentication?.ServiceAccountImpersonationUrl is null &&
+            credentialsJson.RootElement.TryGetProperty("service_account_impersonation_url", out var impersonationUrlElement))
+        {
+            options.FederatedAksAuthentication ??= new();
+            options.FederatedAksAuthentication.ServiceAccountImpersonationUrl = impersonationUrlElement.GetString()!;
+        }
+
+        if (options.BigQueryClient is null && options.ProjectId is { } projectId)
+        {
+            if (credentialsJson.RootElement.TryGetProperty("private_key", out _))
+            {
+                options.BigQueryClient = BigQueryClient.Create(
+                    projectId,
+                    GoogleCredential.FromJson(credentialsJson.ToString()));
+            }
+            else if (Environment.GetEnvironmentVariable(FederatedAksSubjectTokenProvider.TokenPathEnvironmentVariableName) is not null &&
+                options.FederatedAksAuthentication is { Audience: { } audience, ServiceAccountImpersonationUrl: { } serviceAccountImpersonationUrl })
+            {
+                options.BigQueryClient = BigQueryClient.Create(
+                    projectId,
+                    GoogleCredential.FromProgrammaticExternalAccountCredential(
+                        new ProgrammaticExternalAccountCredential(
+                            new ProgrammaticExternalAccountCredential.Initializer(
+                                tokenUrl: "https://sts.googleapis.com/v1/token",
+                                audience,
+                                FederatedAksSubjectTokenProvider.SubjectTokenType,
+#pragma warning disable CA2000
+                                new FederatedAksSubjectTokenProvider())
+                            {
+                                ServiceAccountImpersonationUrl = serviceAccountImpersonationUrl
+                            })));
+#pragma warning restore CA2000
+            }
         }
     }
 }
