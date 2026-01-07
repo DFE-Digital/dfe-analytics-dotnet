ea36b7fce8c801043dcbdde5529d6e83776c5854
James Gunn <james@gunn.io>
2026-01-07 10:49:37 +0000

    Specify the DbContext type name in msbuild property
    This removes the need for marker files to record the assembly the DbContext lives in.

--- src/Dfe.Analytics.EFCore/Cli/DbContextHelper.cs
+++ src/Dfe.Analytics.EFCore/Cli/DbContextHelper.cs
@@ -6,27 +6,18 @@ namespace Dfe.Analytics.EFCore.Cli;
 
 internal static class DbContextHelper
 {
-    public static DbContext CreateDbContext(string dbContextAssemblyPath, string dbContextTypeName)
+    public static DbContext CreateDbContext(string dbContextTypeName)
     {
-        if (!File.Exists(dbContextAssemblyPath))
-        {
-            throw new FileNotFoundException($"The specified DbContext assembly could not be found: '{dbContextAssemblyPath}'.");
-        }
-
-        var appBasePath = Path.GetDirectoryName(dbContextAssemblyPath)!;
+        ArgumentNullException.ThrowIfNull(dbContextTypeName);
 
         AppDomain.CurrentDomain.AssemblyResolve += ResolveAssembly;
 
         try
         {
-            var dbContextAssemblyName = AssemblyName.GetAssemblyName(dbContextAssemblyPath);
-            var dbContextAssembly = Assembly.Load(dbContextAssemblyName);
+            var dbContextType = Type.GetType(dbContextTypeName) ??
+                throw new InvalidOperationException($"Could not load '${dbContextTypeName}'.");
 
-            var dbContextType = dbContextAssembly.GetType(dbContextTypeName) ??
-                throw new InvalidOperationException($"The specified DbContext type '{dbContextTypeName}' could not be found in assembly '{dbContextAssembly.FullName}'.");
-
-            var dbContext = DbContextActivator.CreateInstance(dbContextType);
-            return dbContext;
+            return DbContextActivator.CreateInstance(dbContextType);
         }
         finally
         {
@@ -35,6 +26,7 @@ internal static class DbContextHelper
 
         Assembly? ResolveAssembly(object? sender, ResolveEventArgs args)
         {
+            var appBasePath = AppContext.BaseDirectory;
             var assemblyName = new AssemblyName(args.Name);
 
             var assemblyPath = Path.Combine(appBasePath, assemblyName.Name + ".dll");
